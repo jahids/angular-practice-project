6cf58097f1413bfc61e61e90024577164faa0464
jahids <evanjahid321@gmail.com>
2023-08-07 18:23:39 +0600

    added some routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildComponent } from './Component/child/child.component';
+import { NewoneComponent } from './Component/newone/newone.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path : "child",
+    component : ChildComponent,
+  },
+  {
+    path : "new",
+    component : NewoneComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
